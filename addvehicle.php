09cdb3c9944930a5d325d7f8e42519dadb1c448c
Lewis Mitchell <52708269+MitchelLewis@users.noreply.github.com>
2021-04-30 16:36:27 +0100

    Added some more form validation

--- addvehicle.php
+++ addvehicle.php
@@ -1,12 +1,21 @@
 <?php
     include_once("connectdb.php");
-    $vehicleReg = htmlspecialchars($_POST["reg_no"]);
+    $vehicleReg = htmlspecialchars($_POST["reg_no"]);   
     $vehicleBrand = htmlspecialchars($_POST["brand"]);
     $vehicleDailyRate = htmlspecialchars($_POST["daily_rate"]);
-    $typeOfVehicle = htmlspecialchars($_POST["type_of_vehicle"]);
-    if(!is_numeric($vehicleDailyRate)) {
+    
+    if(empty($vehicleReg)) {
+        echo "<h1>Vehicle reg was not entered</h1>";
+    } elseif(empty($vehicleBrand)) {
+        echo "<h1>Vehicle brand was not entered</h1>";
+    } elseif(empty($vehicleDailyRate)) {
+        echo "<h1>Vehicle daily rate was not entered</h1>";
+    } elseif(empty($_POST["type_of_vehicle"])) {
+        echo "<h1>Type of vehicle was not entered</h1>";
+    } elseif(!is_numeric($vehicleDailyRate)) {
         echo "<h1>Vehicle daily rate was not valid</h1>";
     } else {
+        $typeOfVehicle = htmlspecialchars($_POST["type_of_vehicle"]);
         $sql = "INSERT INTO vehicles VALUES ('$vehicleReg', '$typeOfVehicle', '$vehicleBrand', ' ', $vehicleDailyRate)";
         $stmt = $pdo->prepare($sql);
         $stmt -> execute();
